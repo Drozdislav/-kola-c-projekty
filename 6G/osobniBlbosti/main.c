7075fe3a8ee5f1e56b8025fd8d7645ab11f42f91
Drozdislav <jakubson.drozd@gmail.com>
2025-03-24 15:20:48 +0100

    problem osmi dam dokoncen, tisk reeni ciselnou i obrazkovou formou

--- 6G/osobniBlbosti/main.c
+++ 6G/osobniBlbosti/main.c
@@ -13,27 +13,25 @@ typedef struct
 } TSach;
 
 typedef struct TReseni {
-    int ypozice[8];       // Pole pro ulozeni pozic dam v jednotlivxch sloupcich
+    int ypozice[8];       // Pole pro ulozeni pozic dam v jednotlivych sloupcich
     struct TReseni *next;  // Ukazatel na dalsi reseni v seznamu
 } TReseni;
 
 TReseni *reseniHead = NULL;
 
-void polozDamu(TSach * plocha, int x, int y)
+void polozDamu(TSach *plocha, int x, int y)
 {
     plocha->ypozice[x] = y;
     plocha->jevradku[y] = true;
-    plocha->jesikmozprava[x-y+7] = true;
-    plocha->jesikmozleva[x+y] = true;
+    plocha->jesikmozprava[x - y + 7] = true;
+    plocha->jesikmozleva[x + y] = true;
 }
-
 
 void zapamatuj(TSach *plocha)
 {
-
     TReseni *novyUzel = (TReseni *)malloc(sizeof(TReseni));
     if (novyUzel == NULL) {
-        printf("Chyba alokace pamìti!\n");
+        printf("Chyba alokace pamÄ›ti!\n");
         return;
     }
 
@@ -45,18 +43,17 @@ void zapamatuj(TSach *plocha)
     reseniHead = novyUzel;
 }
 
-
 void odeberDamu(TSach *plocha, int x, int y)
 {
     plocha->ypozice[x] = 0;
     plocha->jevradku[y] = false;
-    plocha->jesikmozprava[x-y+7] = false;
-    plocha->jesikmozleva[x+y] = false;
+    plocha->jesikmozprava[x - y + 7] = false;
+    plocha->jesikmozleva[x + y] = false;
 }
 
 bool jeOhrozena(TSach *plocha, int x, int y)
 {
-    if(plocha->jevradku[y] || plocha->jesikmozleva[x+y] || plocha->jesikmozprava[x-y+7])
+    if (plocha->jevradku[y] || plocha->jesikmozleva[x + y] || plocha->jesikmozprava[x - y + 7])
         return true;
     else
         return false;
@@ -64,13 +61,13 @@ bool jeOhrozena(TSach *plocha, int x, int y)
 
 void zkusSloupec(TSach *plocha, int x)
 {
-    for (int y=0; y<7; y++)
+    for (int y = 0; y < 8; y++)
     {
         if (!jeOhrozena(plocha, x, y))
         {
             polozDamu(plocha, x, y);
 
-            if(x == 7)
+            if (x == 7)
                 zapamatuj(plocha);
             else
                 zkusSloupec(plocha, x + 1);
@@ -82,36 +79,62 @@ void zkusSloupec(TSach *plocha, int x)
 
 void tiskReseniOsmiDam()
 {
-    /**
-    while(TReseni->next != NULL)
+    int volba;
+    printf("-------------------- RESENI OSMI DAM --------------------\n");
+    printf("Zvol si, ktery styl reseni chces, aby ti program vytiskl:\n");
+    scanf("%d", &volba);
+    switch (volba)
     {
-        printf("_________________\n");
-        printf("|");
-        for(int i=0; i<8; i++)
-        {
-            if
-        }
+        case 1:
+            printf("1. Tisk reseni v ciselne forme:\n");
+            TReseni *aktualni1 = reseniHead;
+            int cisloReseni1 = 1;
+            while (aktualni1)
+            {
+                printf("Reseni %d: ", cisloReseni1++);
+                for (int i = 0; i < 8; i++)
+                {
+                    printf("%d ", aktualni1->ypozice[i]);
+                }
+                printf("\n");
+                aktualni1 = aktualni1->next;
+            }
+        break;
 
+        case 2:
+            printf("2. Tisk reseni v obrazkove forme:\n");
+            TReseni *aktualni2 = reseniHead;
+            int cisloReseni2 = 1;
+            while (aktualni2)
+            {
+                printf("Reseni %d:\n", cisloReseni2++);
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (aktualni2->ypozice[j] == i)
+                            printf("[o]");
+                        else
+                            printf("[ ]");
+                    }
+                    printf("\n");
+                }
+
+                printf("\n");
+                aktualni2 = aktualni2->next;
+            }
+            break;
+        default:
+            printf("Neplatna volba. Zadejte 1 nebo 2.\n");
     }
-    **/
-    TReseni *aktualni = reseniHead;
-    int cisloReseni = 1;
-    while (aktualni)
-    {
-        printf("Reseni %d: ", cisloReseni++);
-        for (int i = 0; i < 8; i++)
-        {
-            printf("%d ", aktualni->ypozice[i]);
-        }
-        printf("\n");
-        aktualni = aktualni->next;
-    }
+
+
 }
 
 void testOsmiDam()
 {
-    TSach plocha =
-    {
+    TSach plocha = {
         .ypozice = {0},
         .jevradku = {false},
         .jesikmozleva = {false},
@@ -119,10 +142,7 @@ void testOsmiDam()
     };
 
     zkusSloupec(&plocha, 0);
-
     tiskReseniOsmiDam();
-
-
 }
 
 int main()
