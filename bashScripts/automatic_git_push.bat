5304a9ef924dcc30eed25f488493d1fbfe45b404
Drozdislav <jakubson.drozd@gmail.com>
2025-04-06 21:56:24 +0200

    Auto-commit on Sun 04/06/2025 21:56:24.06

--- bashScripts/automatic_git_push.bat
+++ bashScripts/automatic_git_push.bat
@@ -6,13 +6,32 @@ echo ==========================
 echo.
 echo Pushing latest changes...
 
+@echo off
+setlocal
+
+:: Clean up the time string (remove leading space and colons)
+set timestamp=%date% %time%
+set timestamp=%timestamp: =0%
+set timestamp=%timestamp::=-%
+
+:: Save it to a text file
+echo Current date and time: %timestamp% > current_datetime.txt
+
+echo Written to current_datetime.txt: %timestamp%
+endlocal
+pause
+
+
+set timestamp=%date% %time%
+mkdir %timestamp%
+
 cd "C:\Users\jakubson\Desktop\skola\IVT\Cprojekty\-kola-c-projekty"
 
 REM Add all changes
 git add .
 
 REM Commit with a default message + timestamp
-set timestamp=%date% %time%
+
 git commit -m "Auto-commit on %timestamp%"
 
 REM Push to the main branch
